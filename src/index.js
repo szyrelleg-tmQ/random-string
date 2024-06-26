62b262d337cc87ddddba36faa952be9e5ea121a5
szyrelleg <szyrelle.g@txtmequick.com>
2024-06-26 22:42:44 +0800

    add testing

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { faker } = require('@faker-js/faker');
+
+function generateName() {
+  const firstName = faker.person.fullName();
+  return firstName;
+}
+
+function generateGenre(){
+  const genre = faker.music.genre();
+  return genre;
+}
+
+module.exports = { generateName, generateGenre };
